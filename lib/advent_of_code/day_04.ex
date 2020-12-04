c04efa0f74898e1a97454f8b4f36086184f1b6e1
Roman Roštár <roman.rostar@gmail.com>
2020-12-04 22:43:05 +0100

    WIP day 4 part 2

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,4 +1,16 @@
 defmodule AdventOfCode.Day04 do
+  def parse_passport_input(input_string) do
+      input_string
+      |> String.split("\n\n")
+      |> Enum.map(fn row ->
+        row
+        |> String.split()
+        |> Enum.map(&String.split(&1, ":"))
+        |> Enum.into(%{}, fn [k, v] -> {k, v} end)
+      end)
+  end
+
+  @spec passport_valid?(map) :: boolean
   def passport_valid?(pass) do
     required = [
       # (Birth Year)
@@ -14,7 +26,7 @@ defmodule AdventOfCode.Day04 do
       # (Eye Color)
       "ecl",
       # (Passport ID)
-      "pid",
+      "pid"
       # (Country ID) - ignored for part 1
       # "cid"
     ]
@@ -22,20 +34,76 @@ defmodule AdventOfCode.Day04 do
     required |> Enum.all?(fn x -> pass |> Map.has_key?(x) end)
   end
 
-  def part1(input) do
-    passports =
-      input
-      |> String.split("\n\n")
-      |> Enum.map(fn row ->
-        row
-        |> String.split()
-        |> Enum.map(&String.split(&1, ":"))
-        |> Enum.into(%{}, fn [k, v] -> {k, v} end)
-      end)
+  @spec passport_valid_part2?(map) :: boolean
+  def passport_valid_part2?(pass) do
+    required = [
+      # (Birth Year)
+      "byr" ,
+      # (Issue Year)
+      "iyr",
+      # (Expiration Year)
+      "eyr",
+      # (Height)
+      "hgt",
+      # (Hair Color)
+      "hcl",
+      # (Eye Color)
+      "ecl",
+      # (Passport ID)
+      "pid",
+    ]
 
-    passports |> Enum.count(&passport_valid?/1)
+    required |> Enum.all?(fn x -> pass |> Map.has_key?(x) and passport_part_valid?(x, Map.get(pass, x)) end)
   end
 
-  def part2(args) do
+  # hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+  # ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+  # pid (Passport ID) - a nine-digit number, including leading zeroes.
+  # cid (Country ID) - ignored, missing or not
+
+  defp number_between?(value, lower_bound, upper_bound) do
+    case Integer.parse(value) do
+      :error -> false
+      {parsed_num, _} -> parsed_num >= lower_bound and parsed_num <= upper_bound
+    end
+  end
+
+  def passport_part_valid?("byr", value) do
+    # byr (Birth Year) - four digits; at least 1920 and at most 2002.
+    number_between?(value, 1920, 2002)
+  end
+
+
+  def passport_part_valid?("iyr", value) do
+    # iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+    number_between?(value, 1920, 2020)
+  end
+
+
+  def passport_part_valid?("eyr", value) do
+    # eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+    number_between?(value, 1920, 2020)
+  end
+
+
+  def passport_part_valid?("hgt", value) do
+    # hgt (Height) - a number followed by either cm or in:
+    # If cm, the number must be at least 150 and at most 193.
+    # If in, the number must be at least 59 and at most 76.
+    cpt = Regex.named_captures(~r/(?<h>\d+)(?<unit>(cm)|(in))/, value)
+
+    case cpt do
+      %{"h" => h, "unit" => "cm" } -> number_between?(h, 150, 193)
+      %{"h" => h, "unit" => "in" } -> number_between?(h, 59, 76)
+      _ -> false
+    end
+  end
+
+  def part1(input) do
+    parse_passport_input(input) |> Enum.count(&passport_valid?/1)
+  end
+
+  def part2(input) do
+    parse_passport_input(input) |> Enum.count(&passport_valid_part2?/1)
   end
 end
--- test/advent_of_code/day_04_test.exs
+++ test/advent_of_code/day_04_test.exs
@@ -3,19 +3,74 @@ defmodule AdventOfCode.Day04Test do
 
   import AdventOfCode.Day04
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  test "validate birth year" do
+    assert not passport_part_valid?("byr", "xasda")
 
-    assert result
+    assert not passport_part_valid?("byr", "1919")
+    assert passport_part_valid?("byr", "1920")
+    assert passport_part_valid?("byr", "2002")
+    assert not passport_part_valid?("byr", "2003")
   end
 
-  @tag :skip
-  test "part2" do
-    input = nil
-    result = part2(input)
+  test "validate height" do
+    assert passport_part_valid?("hgt", "60in")
+    assert passport_part_valid?("hgt", "190cm")
+    assert not passport_part_valid?("hgt", "190in")
+    assert not passport_part_valid?("hgt", "190")
+  end
 
-    assert result
+  @tag :wip
+  test "validate hair color" do
+    assert passport_part_valid?("hcl", "#123abc")
+    assert not passport_part_valid?("hcl", "#123abz")
+    assert not passport_part_valid?("hcl", "123abc")
+  end
+
+  test "validate eye color" do
+    assert passport_part_valid?("ecl", "brn")
+    assert not passport_part_valid?("ecl", "wat")
+  end
+
+  test "validate passport id" do
+    assert passport_part_valid?("pid", "000000001")
+    assert not passport_part_valid?("pid", "0123456789")
+  end
+
+  test "part 2 invalid" do
+    input = """
+            eyr:1972 cid:100
+            hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+            iyr:2019
+            hcl:#602927 eyr:1967 hgt:170cm
+            ecl:grn pid:012533040 byr:1946
+
+            hcl:dab227 iyr:2012
+            ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+            hgt:59cm ecl:zzz
+            eyr:2038 hcl:74454a iyr:2023
+            pid:3556412378 byr:2007
+            """
+
+    assert part2(input) == 0
+  end
+
+  test "part 2 valid" do
+    input = """
+            pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+            hcl:#623a2f
+
+            eyr:2029 ecl:blu cid:129 byr:1989
+            iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+            hcl:#888785
+            hgt:164cm byr:2001 iyr:2015 cid:88
+            pid:545766238 ecl:hzl
+            eyr:2022
+
+            iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+            """
+    assert part2(input) == 4
   end
 end
