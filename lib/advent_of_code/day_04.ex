31eee528615f4c97b4031151a187a0dfaf2d5324
Roman Roštár <roman.rostar@gmail.com>
2020-12-04 15:58:54 +0100

    Day 04 part 1

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,5 +1,39 @@
 defmodule AdventOfCode.Day04 do
-  def part1(args) do
+  def passport_valid?(pass) do
+    required = [
+      # (Birth Year)
+      "byr",
+      # (Issue Year)
+      "iyr",
+      # (Expiration Year)
+      "eyr",
+      # (Height)
+      "hgt",
+      # (Hair Color)
+      "hcl",
+      # (Eye Color)
+      "ecl",
+      # (Passport ID)
+      "pid",
+      # (Country ID) - ignored for part 1
+      # "cid"
+    ]
+
+    required |> Enum.all?(fn x -> pass |> Map.has_key?(x) end)
+  end
+
+  def part1(input) do
+    passports =
+      input
+      |> String.split("\n\n")
+      |> Enum.map(fn row ->
+        row
+        |> String.split()
+        |> Enum.map(&String.split(&1, ":"))
+        |> Enum.into(%{}, fn [k, v] -> {k, v} end)
+      end)
+
+    passports |> Enum.count(&passport_valid?/1)
   end
 
   def part2(args) do
