18f138f0192ca99c59df44e1e10eeac59a3e9584
Roman Roštár <roman.rostar@gmail.com>
2020-12-08 23:45:20 +0100

    Day 08 part 2

--- lib/advent_of_code/day_08.ex
+++ lib/advent_of_code/day_08.ex
@@ -3,6 +3,7 @@ defmodule AdventOfCode.Day08 do
     defstruct [:cmd, :op, :count, :effect]
   end
 
+  @spec get_effect(<<_::8>>, any) :: (number -> number)
   def get_effect("+", count) do
     &(&1 + count)
   end
@@ -11,8 +12,7 @@ defmodule AdventOfCode.Day08 do
     &(&1 - count)
   end
 
-  @spec parse_intstructions(binary) :: [any]
-  def parse_intstructions(input) do
+  def parse_instructions(input) do
     input
     |> String.split("\n")
     |> Stream.map(&String.split/1)
@@ -31,27 +31,54 @@ defmodule AdventOfCode.Day08 do
   @spec run_instructions(any, any, any, any) :: any
   def run_instructions(instruction_map, visited, position, acc) do
     if MapSet.member?(visited, position) do
-      acc
+      {:infinite, acc}
     else
       command = instruction_map |> Map.get(position)
 
-      updated_visited = MapSet.put(visited, position)
+      if is_nil(command) do
+        {:ok, acc}
+      else
+        updated_visited = MapSet.put(visited, position)
 
-      case command.cmd do
-        "nop" -> run_instructions(instruction_map, updated_visited, position + 1, acc)
-        "jmp" -> run_instructions(instruction_map, updated_visited, command.effect.(position), acc)
-        "acc" -> run_instructions(instruction_map, updated_visited, position + 1, command.effect.(acc))
+        case command.cmd do
+          "nop" ->
+            run_instructions(instruction_map, updated_visited, position + 1, acc)
+
+          "jmp" ->
+            run_instructions(instruction_map, updated_visited, command.effect.(position), acc)
+
+          "acc" ->
+            run_instructions(instruction_map, updated_visited, position + 1, command.effect.(acc))
+        end
       end
     end
   end
 
-  @spec part1(binary) :: :ok | {:error, :no_iex | :refused}
   def part1(input) do
-    instruction_map = input |> parse_intstructions
+    instruction_map = input |> parse_instructions
 
     run_instructions(instruction_map, MapSet.new(), 0, 0)
   end
 
+  def swap_instruction(cmd) do
+    case cmd do
+      %Command{cmd: "jmp"} -> %Command{cmd | cmd: "nop"}
+      %Command{cmd: "nop"} -> %Command{cmd | cmd: "jmp"}
+    end
+  end
+
   def part2(input) do
+    instructions = input |> parse_instructions
+
+    instructions
+    |> Stream.map(fn
+      {_, %Command{cmd: "acc"}} -> nil
+      {index, command} -> Map.put(instructions, index, swap_instruction(command))
+    end)
+    |> Stream.filter(&(not is_nil(&1)))
+    |> Stream.map(&run_instructions(&1, MapSet.new(), 0, 0))
+    |> Stream.filter(fn {type, _} -> type == :ok end)
+    |> Enum.to_list()
+    |> List.first()
   end
 end
