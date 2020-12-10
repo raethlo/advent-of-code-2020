e8198dc2deb2b1c7278eade5946a0ed12fd08e72
Roman Roštár <roman.rostar@gmail.com>
2020-12-11 00:58:15 +0100

    Day 10 part 1

--- lib/advent_of_code/day_10.ex
+++ lib/advent_of_code/day_10.ex
@@ -1,7 +1,19 @@
 defmodule AdventOfCode.Day10 do
-  def part1(args) do
+  def parse_input(input) do
+    input |> String.split("\n") |> Stream.map(&Integer.parse/1) |> Stream.map(fn {n, _} -> n end) |> Enum.to_list()
   end
 
-  def part2(args) do
+  def count_jolt_diff_chain(numbers) do
+    sorted = [0 | numbers] |> Enum.sort(:desc)
+    [first | _] = sorted
+    [first+3 | sorted] |> Stream.chunk_every(2,1, :discard) |> Stream.map(fn [a,b] -> a - b end) |> Enum.frequencies()
+  end
+
+  def part1(input) do
+    freqs = input |> parse_input() |> count_jolt_diff_chain()
+    freqs[1] * freqs[3]
+  end
+
+  def part2(intput) do
   end
 end
--- lib/mix/tasks/d10.p1.ex
+++ lib/mix/tasks/d10.p1.ex
@@ -5,7 +5,7 @@ defmodule Mix.Tasks.D10.P1 do
 
   @shortdoc "Day 10 Part 1"
   def run(args) do
-    input = nil
+    input = File.read!("data/10/input.txt")
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_1: fn -> input |> part1() end}),
