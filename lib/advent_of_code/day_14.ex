8b1c9ff0adcb91b425e3b0bf7f6c588705a5103e
Roman Roštár <roman.rostar@gmail.com>
2020-12-15 00:46:38 +0100

    Day 14 part 1 complete

--- lib/advent_of_code/day_14.ex
+++ lib/advent_of_code/day_14.ex
@@ -1,6 +1,31 @@
 defmodule AdventOfCode.Day14 do
   use Bitwise
 
+  def permutations([]), do: [[]]
+  def permutations(list), do: for elem <- list, rest <- permutations(list--[elem]), do: [elem|rest]
+
+  def consume(instruction_list, mask, acc)
+  def consume([], _, acc) do
+    acc
+  end
+  def consume([h | t], mask, acc) do
+    [instr, value] = String.split(h, " = ")
+
+    case String.slice(instr, 0, 3) do
+      "mas" ->
+        new_mask = read_mask(value)
+        consume(t, new_mask, acc)
+
+      "mem" ->
+        %{"mem" => mem} = Regex.named_captures(~r/mem\[(?<mem>\d+)\]/, instr)
+
+        masked_value = apply_mask(String.to_integer(value), mask)
+        new_acc = Map.put(acc, String.to_integer(mem), masked_value)
+
+        consume(t, mask, new_acc)
+    end
+  end
+
   def read_mask(string) do
     %{
       zero: string |> String.replace("X", "1") |> String.to_integer(2),
@@ -8,14 +33,28 @@ defmodule AdventOfCode.Day14 do
     }
   end
 
+  def expand_floating_bits(masks, acc \\ [])
+  def expand_floating_bits([], acc), do: acc
+  def expand_floating_bits([h | t], acc)  do
+
+    if String.contains?(h, "X") do
+      expand_floating_bits([String.replace(h, "X", "1", global: false), String.replace(h, "X", "0", global: false) | t ], acc)
+    else
+      expand_floating_bits(t, [h | acc])
+    end
+  end
 
   def apply_mask(number, %{zero: zero_mask, one: one_mask}) do
     (number ||| one_mask) &&& zero_mask
   end
 
-  def part1(args) do
+  def part1(input) do
+    instruction_list = String.split(input, "\n")
+    memory = consume(instruction_list, "", %{})
+    memory |> Map.values |> Enum.filter(& &1 > 0) |> Enum.sum
   end
 
   def part2(args) do
+    require IEx;IEx.pry
   end
 end
--- test/advent_of_code/day_14_test.exs
+++ test/advent_of_code/day_14_test.exs
@@ -4,18 +4,20 @@ defmodule AdventOfCode.Day14Test do
   import AdventOfCode.Day14
 
   test "Part 1 do" do
-    input = """
-    mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
-    mem[8] = 11
-    mem[7] = 101
-    mem[8] = 0
-    """
+    input =
+      """
+      mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+      mem[8] = 11
+      mem[7] = 101
+      mem[8] = 0
+      """
+      |> String.trim_trailing()
 
     assert part1(input) === 165
   end
 
   test "read mask" do
-    assert read_mask("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X") == %{one: 64, zero: 68719476733}
+    assert read_mask("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X") == %{one: 64, zero: 68_719_476_733}
   end
 
   test "apply mask" do
@@ -23,6 +25,12 @@ defmodule AdventOfCode.Day14Test do
     assert apply_mask(11, mask) == 73
     assert apply_mask(101, mask) == 101
     assert apply_mask(0, mask) == 64
+  end
+
+  test "expand floating bits" do
+    mask = "X0X1"
+
+    assert MapSet.new(expand_floating_bits([mask])) == MapSet.new(["1011","0001","1001","0011"])
   end
 
   @tag :skip
